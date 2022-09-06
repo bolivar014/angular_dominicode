caedf5c3decdc8d075b25cef0dd70450dd625742
Camilo Valdes <bolivar014@gmail.com>
2022-09-06 09:22:25 -0500

    envio de Input selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnChanges, OnInit, OnDestroy {
+export class AppComponent {
   name?: string;
   title = 'Día 5 del reto';
   url = 'https://d.wattpad.com/story_parts/2/images/15948b36ef404a19901251797207.jpg';
@@ -14,22 +14,6 @@ export class AppComponent implements OnChanges, OnInit, OnDestroy {
 
   //
   cities = ['Colombia', 'Barcelona', 'Perú', 'Ecuador'];
-
-  // Función que retorna los cambios
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log('Changes... -> ', changes);
-  }
-
-  // Metodo que siempre se ejecuta de primero cuando no existe Input y Output
-  // Se utiliza para consumir las apis - antes de cargar las vistas
-  ngOnInit(): void {
-    console.log('ngOnInit... -> ');
-  }
-
-  // Se ejecuta cuando se destruye el componente...
-  ngOnDestroy(): void {
-    console.log('Destroy... -> ');
-  }
 
   // Evento para determinar los click en la lista tipo UL
   onCityClicked(city: string): void {
