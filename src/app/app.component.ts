5f5e21c2cc24ee4e80887d3270a84640dc5158dd
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-09-05 20:15:51 -0500

    ejemplo de los ciclos de vida de los componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges, OnInit, OnDestroy {
   name?: string;
   title = 'Día 5 del reto';
   url = 'https://d.wattpad.com/story_parts/2/images/15948b36ef404a19901251797207.jpg';
 
   //
   cities = ['Colombia', 'Barcelona', 'Perú', 'Ecuador'];
+
+  // Función que retorna los cambios
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Changes... -> ', changes);
+  }
+
+  // Metodo que siempre se ejecuta de primero cuando no existe Input y Output
+  // Se utiliza para consumir las apis - antes de cargar las vistas
+  ngOnInit(): void {
+    console.log('ngOnInit... -> ');
+  }
+
+  // Se ejecuta cuando se destruye el componente...
+  ngOnDestroy(): void {
+    console.log('Destroy... -> ');
+
+  }
 }
