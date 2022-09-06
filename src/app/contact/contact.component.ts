169282f667f598fd5637f1a759247889d55ff6fc
Camilo Valdes <bolivar014@gmail.com>
2022-09-06 13:22:03 -0500

    creando evento submit - exportar datos de formulario

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -12,4 +12,9 @@ export class ContactComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  // Evento submit
+  onSubmit(values: any): void {
+    console.log('Form values', values);
+  }
 }
