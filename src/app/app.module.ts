01aa4df67b49fdddf428efb0e19ebfc9b3a547b4
Camilo Valdes <bolivar014@gmail.com>
2022-09-06 11:32:34 -0500

    creación de pipe personalizado - filtro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from './button/button.component';
 import { FormNewItemComponent } from './form-new-item/form-new-item.component';
 import { CitiesComponent } from './cities/cities.component';
+// Importamos pipe
+import { FilterPipe } from './pipes/filter.pipe';
+
 @NgModule({
   declarations: [
     AppComponent,
     ButtonComponent,
     FormNewItemComponent,
-    CitiesComponent
+    CitiesComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
