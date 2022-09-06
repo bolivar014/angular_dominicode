2f4da8a8154eaff2422321467a26b992006f8e4b
Camilo Valdes <bolivar014@gmail.com>
2022-09-06 13:28:48 -0500

    integrando two-way data binding en formulario contacto

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+// Creamos interface
+interface ContactoForm {
+  "name": string;
+  "checkAdult": boolean;
+  "departament": string;
+  "comment": string;
+}
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
+  // modelo
+  model = {
+    "name": "",
+    "checkAdult": false,
+    "departament": "",
+    "comment": ""
+  }
   constructor() { }
 
   ngOnInit(): void {
